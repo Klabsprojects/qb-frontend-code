224d580e574dc7a81253dacacdbf5d1d0ae6acda
Manimaranboss <manimaran@klabsindia.com>
2024-03-28 17:53:22 +0530

    changes : Question paper edit done

--- src/app/user/shared.service.ts
+++ src/app/user/shared.service.ts
@@ -37,8 +37,27 @@ export class shared {
         const newtopicUrl = 'https://api-qgen.a2zweb.in/api/master/detail/' + selectedClass + '/' + selectedSubject + '/' + selectedChapter
         return this.http.get<any[]>(newtopicUrl, { headers: this.getHeaders() });
     }
-    search_questions(payload:any) {
+    search_questions(payload: any) {
         const searchUrl = environment.baseUrl + `/api/question/search/data?type=${payload.type}&class=${payload.class}&subject=${payload.subject}&chapter=${payload.chapter}&difficulty=${payload.level}&format=${payload.qtype}&topic=${payload.topic}`
         return this.http.get<any[]>(searchUrl, { headers: this.getHeaders() });
     }
+    save_question_paper(payload: any) {
+        const newQuestionUrl = 'https://api-qgen.a2zweb.in/api/question/paper/new';
+        return this.http.post(newQuestionUrl, payload, { headers: this.getHeaders() });
+    }
+    update_question_paper(payload:any,id:any){
+        const newupdateurl = 'https://api-qgen.a2zweb.in/api/question/paper/'+id;
+        return this.http.put(newupdateurl,payload,{ headers: this.getHeaders() })
+    }
+    get_questionpaper_list() {
+        const getquestionpaperlist = 'https://api-qgen.a2zweb.in/api/question/paper/list';
+        return this.http.get<any[]>(getquestionpaperlist, { headers: this.getHeaders() })
+    }
+    get_questions(qIds: number[] | number) {
+        const ids = Array.isArray(qIds) ? qIds.join(',') : qIds.toString();
+        const params = new HttpParams().set('ids', ids);
+        var getReportUrl = 'https://api-qgen.a2zweb.in/api/question/selected/data';
+        return this.http.get(getReportUrl, { params, headers: this.getHeaders() });
+    }
+
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { shared } from './shared.service';
 
 @Component({
@@ -31,8 +31,11 @@ export class UserComponent implements OnInit {
   public englishselected: boolean = true;
   public selectedQuestionIds: any[] = [];
   public choosenquestions: any[] = [];
-  public draftquestionpaper:any = {"questionpapername":'',"questionpaperid":'','English':false,'Tamil':false,ids:[]};
-  public draftquestionpaperlist:any = [{"questionpapername":'Model Exam',"questionpaperid":'',ids:[]},{"questionpapername":'Half yearly',"questionpaperid":'',ids:[]}];
+  public draftquestionpaper: any = { "name": '', "details": '', ids: [] };
+  public drafteditquestion: any = { "name": '', "details": '', ids: [] };
+  public draftquestionpaperlist: any = [];
+  public editable_question: any = { name: "", detail: "", questions: [] }
+  @ViewChild('exampleModal') modal!: ElementRef;
   ngOnInit(): void {
     // var payload = { 'type': 'NEET', 'class': '11', 'subject': 'Maths', 'level': 'Easy', 'qtype': 1, 'chapter': 'Integral Calculus', 'topic': 'Introduction' }
     // this.shared.search_questions(payload).subscribe((res: any) => {
@@ -40,6 +43,7 @@ export class UserComponent implements OnInit {
     //   this.totalquestions = res.data
     //   console.log('resdata', res.data)
     // })
+    this.get_questionpaper()
   }
   selectClass(event: any) {
     this.type = event.target.value
@@ -288,9 +292,9 @@ export class UserComponent implements OnInit {
   }
   generatequestion_solution(data: any[]) {
     var htmlContent: any;
-    if(this.englishselected && !this.tamilselected){
+    if (this.englishselected && !this.tamilselected) {
       htmlContent = data.map((question: any, index: number) =>
-      `<p><strong>Question ${index + 1}:</strong> ${question.text}</p>
+        `<p><strong>Question ${index + 1}:</strong> ${question.text}</p>
     <strong>Choices : </strong> 
     <div>
       ${question.choices[0].choice_text}       ${question.choices[1].choice_text} 
@@ -303,7 +307,7 @@ export class UserComponent implements OnInit {
     <br>`).join('');
     }
 
-    if (!this.englishselected && this.tamilselected){
+    if (!this.englishselected && this.tamilselected) {
       htmlContent = data.map((question: any, index: number) =>
         `<p><strong>கேள்வி ${index + 1}:</strong> ${question.text_tn}</p>
       <strong>தேர்வுகள் : </strong> 
@@ -319,9 +323,9 @@ export class UserComponent implements OnInit {
       ).join('');
     }
 
-    if(this.englishselected && this.tamilselected){
+    if (this.englishselected && this.tamilselected) {
       htmlContent = data.map((question: any, index: number) =>
-      `<p><strong>Question ${index + 1}:</strong> ${question.text}</p>
+        `<p><strong>Question ${index + 1}:</strong> ${question.text}</p>
     <strong>Choices : </strong> 
     <div>
       ${question.choices[0].choice_text}       ${question.choices[1].choice_text} 
@@ -364,13 +368,69 @@ export class UserComponent implements OnInit {
   filterChoices(choices: any[]) {
     return choices.filter(choice => choice.choice_correct_yn !== null)[0];
   }
-  addquestion(qid:any){
+  addquestion(qid: any) {
     this.draftquestionpaper.ids.push(qid)
   }
-  savequestionpaper(name:any){
-    this.draftquestionpaper.questionpapername = name.value
-    this.draftquestionpaper.English = this.englishselected
-    this.draftquestionpaper.Tamil = this.tamilselected
-    console.log("this.draftquestionpaper",this.draftquestionpaper)
+  savequestionpaper(name: any) {
+    const uniqueIds = [...new Set(this.drafteditquestion['ids'])];
+    let question_paper = { 'name': name.value, "detail": "", "questions": uniqueIds.join(',') }
+    this.shared.save_question_paper(question_paper).subscribe((res: any) => {
+      if (res.message = 'Question Paper Created Successfully') {
+        alert("Question Saved Successfully");
+      }
+    })
+  }
+  updatequestionpaper(name:any){
+    const uniqueIds = [...new Set(this.drafteditquestion['ids'])];
+    let question_paper = { 'name': name.value, "detail": "", "questions": uniqueIds.join(',') }
+    this.shared.update_question_paper(question_paper,this.editable_question['id']).subscribe((res:any)=>{
+      console.log("res",res)
+      alert("Question Paper Updated Success")
+    })
+  }
+  get_questionpaper() {
+    this.shared.get_questionpaper_list().subscribe((res: any) => {
+      this.draftquestionpaperlist = res.data;
+    })
+  }
+  retrivequestions(questions: string) {
+    let array = questions.split(",").map(Number);
+    this.shared.get_questions(array).subscribe((res: any) => {
+      console.log("res.data", res.data)
+      // this.generatequestion_solution(res.data)
+    })
+  }
+  updatequestion(question: any) {
+    this.editable_question['id'] = question.id
+    this.editable_question['name'] = question.name
+    let array = question.questions.split(",").map(Number)
+    this.shared.get_questions(array).subscribe((res: any) => {
+      res.data.forEach((obj: any) => {
+        obj.editselected = true;
+      });
+      if (this.totalquestions.length>0){
+        this.editable_question['questions'] = this.totalquestions.map(obj => {
+          let selectedObj = res.data.find((item: any) => item.id === obj.id);
+          if (selectedObj) {
+            return { ...obj, editselected: true };
+          } else {
+            return obj;
+          }
+        });
+        for(let i = 0;i<this.editable_question['questions'].length;i++){
+          if(this.editable_question['questions'][i].editselected){
+            this.drafteditquestion['ids'].push(this.editable_question['questions'][i].id)
+          }
+        }
+      }
+      else{
+        this.editable_question['questions'] =  res.data;
+      }
+      // console.log("this.editable_question['questions']",this.editable_question['questions'])
+    })
+  }
+  editquestion(id:any){
+    this.drafteditquestion['ids'].push(id)
+    console.log("this.drafteditquestion",this.drafteditquestion)
   }
 }
