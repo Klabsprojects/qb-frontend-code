6cd14a294dff1840e91f4f735bf1e1a8bf14c4ad
Manimaranboss <manimaran@klabsindia.com>
2024-04-10 14:35:55 +0530

    changes: sanitize bug resolved temp

--- src/app/user/user/filters/filter-santize.pipe.ts
+++ src/app/user/user/filters/filter-santize.pipe.ts
@@ -2,12 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'sanitizeHtml',
+  name: 'sanitizeHtmluser',
 })
 export class SanitizeHtmlUserPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
 
   transform(html: string): SafeHtml {
+    console.log('html',html)
     return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 }
